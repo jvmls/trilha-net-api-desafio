f51a3cf083293f9e4e3b41f3add0d8c6a7b82fcb
jvmls <jvmls@cesar.school>
2025-09-16 14:59:41 -0300

    Completa CRUD Tarefa, adiciona migrations e documentação

---
+++ migra.sql
@@ -0,0 +1,30 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Tarefas] (
+    [Id] int NOT NULL IDENTITY,
+    [Titulo] nvarchar(max) NULL,
+    [Descricao] nvarchar(max) NULL,
+    [Data] datetime2 NOT NULL,
+    [Status] int NOT NULL,
+    CONSTRAINT [PK_Tarefas] PRIMARY KEY ([Id])
+);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250916173720_InitialCreate', N'6.0.5');
+GO
+
+COMMIT;
+GO
+
